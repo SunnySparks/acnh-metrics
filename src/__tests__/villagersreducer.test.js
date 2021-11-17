72dac0592486cd63d0993530d0d5e5b08501a910
SunnySparks <Josediazt6@outlook.com>
2021-11-17 16:07:58 -0600

    Mock data pull from villagers

--- src/__tests__/villagersreducer.test.js
+++ src/__tests__/villagersreducer.test.js
@@ -1,50 +1,30 @@
 import villagersReducer from '../redux/villagers/villagers';
-const axios = require('axios');
-const getVillagers = require('../redux/villagers/villagers');
-// const initialState = [];
-const axios = require('axios');
 
-it('returns the title of the first album', async () => {
-  axios.get.mockResolvedValue({
-    data: [
-      {
-        name: 'Vladimir',
-        id: 71,
-        birthday: '2/8',
-        species: 'Cub'
-      },
-      {
-        name: 'Raymond',
-        id: 64,
-        birthday: '1/10',
-        species: 'Cat'
-      },
-      {
-        name: 'Spike',
-        id: 339,
-        birthday: '17/6',
-        species: 'Rhino'
-      },
-      {
-        name: 'Shari',
-        id: 250,
-        birthday: '10/4',
-        species: 'Monkey'
-      },
-      {
-        name: 'Maelle',
-        id: 130,
-        birthday: '8/4',
-        species: 'Duck'
-      },
-    ]
-  });
+const prevState = [];
 
-  test('Testing initial state for villager reducer', () => {
+const getVillagers = () => {
+  const data =
+      {
+        "birthday": "2/8",
+        "id": 71,
+        "name": "Vladimir",
+        "species": "Cub",
+      };
+    return prevState.push(data);
+};
+
+describe('Mock villager data pull', () => {
+  test('It returns an empty array before fetching data', () => {
     expect(villagersReducer(undefined, {})).toEqual([]);
   });
-  test('it fetches the name of a villager', () => {
-    const name = await getVillagers();
-    expect(name).toEqual('Vladimir');
+  test('It will fetch the data of the villagers', () => {
+    expect(villagersReducer(prevState, getVillagers())).toEqual(
+        [{
+          "birthday": "2/8",
+          "id": 71,
+          "name": "Vladimir",
+          "species": "Cub",
+        }],
+    )
   })
-});
+})
